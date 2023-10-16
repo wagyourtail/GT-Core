a4d8765ea98dbdc117ada53147e0dfab977f965d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-10-15 20:09:33 -0400

    added remappig for gtrubber items

--- common/src/main/java/io/github/gregtechintergalactical/gtcore/GTCoreRemapping.java
+++ common/src/main/java/io/github/gregtechintergalactical/gtcore/GTCoreRemapping.java
@@ -1,11 +1,15 @@
 package io.github.gregtechintergalactical.gtcore;
 
+import io.github.gregtechintergalactical.gtcore.data.GTCoreItems;
 import io.github.gregtechintergalactical.gtcore.machine.DrumMachine;
 import io.github.gregtechintergalactical.gtcore.machine.LockerMachine;
 import io.github.gregtechintergalactical.gtcore.machine.WorkbenchMachine;
 import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.AntimatterRemapping;
+import muramasa.antimatter.machine.BlockMachine;
+import muramasa.antimatter.registration.IAntimatterObject;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
 public class GTCoreRemapping {
 
@@ -19,5 +23,12 @@ public class GTCoreRemapping {
         AntimatterAPI.all(LockerMachine.class, GTCore.ID).forEach(m -> {
             AntimatterRemapping.remapMachine(new ResourceLocation("gtutility", m.getId()), m);
         });
+        AntimatterAPI.all(Block.class, GTCore.ID).stream().filter(b -> b instanceof IAntimatterObject && !(b instanceof BlockMachine)).map(b -> (IAntimatterObject)b).forEach(b -> {
+            AntimatterRemapping.remap(new ResourceLocation("gtrubber", b.getId()), b.getLoc());
+        });
+        AntimatterRemapping.remapBlockEntity(new ResourceLocation("gtrubber", "sap_bag"), new ResourceLocation(GTCore.ID, "sap_bag"));
+        AntimatterRemapping.remap(new ResourceLocation("gtrubber", "sticky_resin"), GTCoreItems.StickyResin.getLoc());
+        AntimatterRemapping.remap(new ResourceLocation("gtrubber", "rubber_boat"), GTCoreItems.RubberBoat.getLoc());
+        AntimatterRemapping.remap(new ResourceLocation("gtrubber", "rubber_lumber"), new ResourceLocation(GTCore.ID, "rubber_lumber"));
     }
 }
