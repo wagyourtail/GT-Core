3250aa0ab731051f8db62afc0ad4de8dd40f17b1
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-10-17 18:23:20 -0400

    fixed crash on forge and fabric

--- forge/src/main/java/io/github/gregtechintergalactical/gtcore/data/forge/GTCoreBlocksImpl.java
+++ forge/src/main/java/io/github/gregtechintergalactical/gtcore/data/forge/GTCoreBlocksImpl.java
@@ -3,7 +3,7 @@ package io.github.gregtechintergalactical.gtcore.data.forge;
 
 import io.github.gregtechintergalactical.gtcore.integration.tfc.TFCRubberData;
 
-public class GTCoreDataImpl {
+public class GTCoreBlocksImpl {
     public static void initTFC(){
         TFCRubberData.init();
     }
