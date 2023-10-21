a4995181b1c4cce4009ae1329c645355125613b0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-10-20 22:01:19 -0400

    moved over molds and stone types

--- common/src/main/java/io/github/gregtechintergalactical/gtcore/data/GTCoreBlocks.java
+++ common/src/main/java/io/github/gregtechintergalactical/gtcore/data/GTCoreBlocks.java
@@ -11,6 +11,8 @@ import io.github.gregtechintergalactical.gtcore.tree.block.*;
 import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.block.BlockBasic;
 import muramasa.antimatter.material.Material;
+import muramasa.antimatter.ore.CobbleStoneType;
+import muramasa.antimatter.ore.StoneType;
 import muramasa.antimatter.texture.Texture;
 import muramasa.antimatter.util.AntimatterPlatformUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,6 +20,8 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.WoodType;
+
+import static io.github.gregtechintergalactical.gtcore.data.GTCoreMaterials.*;
 
 public class GTCoreBlocks {
 
@@ -55,6 +59,18 @@ public class GTCoreBlocks {
 
 
     public static final BlockEntityType<?> SAP_BAG_BLOCK_ENTITY = BlockEntityType.Builder.of(BlockEntitySapBag::new, SAP_BAG).build(null);
+
+    public static StoneType GRANITE_RED = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "red_granite", RedGranite, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(4.5F, 60.0F).setHarvestLevel(3);
+    public static StoneType GRANITE_BLACK = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "black_granite", BlackGranite, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(4.5F, 60.0F).setHarvestLevel(3);
+    public static StoneType MARBLE = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "marble", Marble, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(0.75F,7.5F);
+
+    public static StoneType KOMATIITE = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "komatiite", Komatiite, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(3.0F, 30.0F).setHarvestLevel(2);
+    public static StoneType LIMESTONE = AntimatterAPI.register(StoneType.class,  new CobbleStoneType(GTCore.ID, "limestone", Limestone, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(0.75F,7.5F);
+    public static StoneType GREEN_SCHIST = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "green_schist", GreenSchist, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(0.75F,7.5F);
+    public static StoneType BLUE_SCHIST = AntimatterAPI.register(StoneType.class,  new CobbleStoneType(GTCore.ID, "blue_schist", BlueSchist, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(0.75F,7.5F);
+    public static StoneType KIMBERLITE = AntimatterAPI.register(StoneType.class,  new CobbleStoneType(GTCore.ID, "kimberlite", Kimberlite, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(3.0F, 30.0F).setHarvestLevel(2);
+    public static StoneType QUARTZITE = AntimatterAPI.register(StoneType.class, new CobbleStoneType(GTCore.ID, "quartzite", Quartzite, "block/stone/", SoundType.STONE, true)).setHardnessAndResistance(0.75F,7.5F);
+    
 
     public static void init() {
         if (!AntimatterAPI.isModLoaded("tfc")){
--- common/src/main/java/io/github/gregtechintergalactical/gtcore/data/GTCoreItems.java
+++ common/src/main/java/io/github/gregtechintergalactical/gtcore/data/GTCoreItems.java
@@ -16,6 +16,12 @@ public class GTCoreItems {
     public static ItemBasic<?> CoalBall = new ItemBasic<>(GTCore.ID, "coal_ball");
     public static ItemBasic<?> CompressedCoalBall = new ItemBasic<>(GTCore.ID, "compressed_coal_ball");
     public static ItemBasic<?> CoalChunk = new ItemBasic<>(GTCore.ID, "coal_chunk");
+    public static ItemBasic<?> DiamondSawBlade = new ItemBasic<>(GTCore.ID, "diamond_saw_blade");
+    public static ItemBasic<?> DiamondGrindHead = new ItemBasic<>(GTCore.ID, "diamond_grind_head");
+    public static ItemBasic<?> TungstenGrindHead = new ItemBasic<>(GTCore.ID, "tungsten_grind_head");
+    public static ItemBasic<?> IridiumAlloyIngot = new ItemBasic<>(GTCore.ID, "iridium_alloy_ingot").tip("Used to make Iridium Plates");
+    public static ItemBasic<?> IridiumReinforcedPlate = new ItemBasic<>(GTCore.ID, "iridium_reinforced_plate").tip("GT2s Most Expensive Component");
+    public static ItemBasic<?> IridiumNeutronReflector = new ItemBasic<>(GTCore.ID, "iridium_neutron_reflector").tip("Indestructible");
 
     public static ItemBasic<?> CircuitBoardBasic = new ItemBasic<>(GTCore.ID, "basic_circuit_board", "circuits/");
     public static ItemBasic<?> CircuitBoardAdvanced = new ItemBasic<>(GTCore.ID, "advanced_circuit_board", "circuits/");
@@ -55,6 +61,43 @@ public class GTCoreItems {
     public static ItemBasic<?> LapotronCrystal = new ItemMultiTextureBattery(GTCore.ID, "lapotron_crystal", Tier.EV, 10_000_000, true);
     public static ItemBasic<?> BatteryEnergyOrb = new ItemMultiTextureBattery(GTCore.ID, "lapotronic_energy_orb", Tier.IV, 100_000_000, true);
     public static ItemBasic<?> BatteryEnergyOrbCluster = new ItemMultiTextureBattery(GTCore.ID, "lapotronic_energy_orb_cluster", Tier.LUV, 1_000_000_000, true);
+
+    public static ItemBasic<?> EmptyShape = new ItemBasic<>(GTCore.ID, "empty_shape_plate", "molds/").tip("Raw plate to make Molds and Extruder Shapes");
+    public static ItemBasic<?> MoldPlate = new ItemBasic<>(GTCore.ID, "plate_mold", "molds/").tip("Mold for making Plates");
+    public static ItemBasic<?> MoldCasing = new ItemBasic<>(GTCore.ID, "casing_mold", "molds/").tip("Mold for making Item Casings");
+    public static ItemBasic<?> MoldGear = new ItemBasic<>(GTCore.ID, "gear_mold", "molds/").tip("Mold for making Gears");
+    public static ItemBasic<?> MoldGearSmall = new ItemBasic<>(GTCore.ID, "small_gear_mold", "molds/").tip("Mold for making Small Gears");
+    public static ItemBasic<?> MoldCoinage = new ItemBasic<>(GTCore.ID, "coinage_mold", "molds/").tip("Secure Mold for making Coins (Don't lose it!)");
+    public static ItemBasic<?> MoldBottle = new ItemBasic<>(GTCore.ID, "bottle_mold", "molds/").tip("Mold for making Bottles");
+    public static ItemBasic<?> MoldIngot = new ItemBasic<>(GTCore.ID, "ingot_mold", "molds/").tip("Mold for making Ingots");
+    public static ItemBasic<?> MoldBall = new ItemBasic<>(GTCore.ID, "ball_mold", "molds/").tip("Mold for making Balls");
+    public static ItemBasic<?> MoldBlock = new ItemBasic<>(GTCore.ID, "block_mold", "molds/").tip("Mold for making Blocks");
+    public static ItemBasic<?> MoldNugget = new ItemBasic<>(GTCore.ID, "nugget_mold", "molds/").tip("Mold for making Nuggets");
+    public static ItemBasic<?> MoldAnvil = new ItemBasic<>(GTCore.ID, "anvil_mold", "molds/").tip("Mold for making Anvils");
+    public static ItemBasic<?> ShapePlate = new ItemBasic<>(GTCore.ID, "plate_shape", "molds/").tip("Shape for making Plates");
+    public static ItemBasic<?> ShapeCasing = new ItemBasic<>(GTCore.ID, "casing_shape", "molds/").tip("Shape for making Item Casings");
+    public static ItemBasic<?> ShapeRod = new ItemBasic<>(GTCore.ID, "rod_shape", "molds/").tip("Shape for making Rods");
+    public static ItemBasic<?> ShapeBolt = new ItemBasic<>(GTCore.ID, "bolt_shape", "molds/").tip("Shape for making Bolts");
+    public static ItemBasic<?> ShapeRing = new ItemBasic<>(GTCore.ID, "ring_shape", "molds/").tip("Shape for making Rings");
+    public static ItemBasic<?> ShapeCell = new ItemBasic<>(GTCore.ID, "cell_shape", "molds/").tip("Shape for making Cells");
+    public static ItemBasic<?> ShapeIngot = new ItemBasic<>(GTCore.ID, "ingot_shape", "molds/").tip("Shape for making Ingots");
+    public static ItemBasic<?> ShapeWire = new ItemBasic<>(GTCore.ID, "wire_shape", "molds/").tip("Shape for making Wires");
+    public static ItemBasic<?> ShapePipeTiny = new ItemBasic<>(GTCore.ID, "tiny_pipe_shape", "molds/").tip("Shape for making Tiny Pipes");
+    public static ItemBasic<?> ShapePipeSmall = new ItemBasic<>(GTCore.ID, "small_pipe_shape", "molds/").tip("Shape for making Small Pipes");
+    public static ItemBasic<?> ShapePipeNormal = new ItemBasic<>(GTCore.ID, "normal_pipe_shape", "molds/").tip("Shape for making Normal Pipes");
+    public static ItemBasic<?> ShapePipeLarge = new ItemBasic<>(GTCore.ID, "large_pipe_shape", "molds/").tip("Shape for making Large Pipes");
+    public static ItemBasic<?> ShapePipeHuge = new ItemBasic<>(GTCore.ID, "huge_pipe_shape", "molds/").tip("Shape for making Huge Pipes");
+    public static ItemBasic<?> ShapeBlock = new ItemBasic<>(GTCore.ID, "block_shape", "molds/").tip("Shape for making Blocks");
+    public static ItemBasic<?> ShapeHeadSword = new ItemBasic<>(GTCore.ID, "sword_head_shape", "molds/").tip("Shape for making Sword Blades");
+    public static ItemBasic<?> ShapeHeadPickaxe = new ItemBasic<>(GTCore.ID, "pickaxe_head_shape", "molds/").tip("Shape for making Pickaxe Heads");
+    public static ItemBasic<?> ShapeHeadShovel = new ItemBasic<>(GTCore.ID, "shovel_head_shape", "molds/").tip("Shape for making Shovel Heads");
+    public static ItemBasic<?> ShapeHeadAxe = new ItemBasic<>(GTCore.ID, "axe_head_shape", "molds/").tip("Shape for making Axe Heads");
+    public static ItemBasic<?> ShapeHeadHoe = new ItemBasic<>(GTCore.ID, "hoe_head_shape", "molds/").tip("Shape for making Hoe Heads");
+    public static ItemBasic<?> ShapeHeadHammer = new ItemBasic<>(GTCore.ID, "hammer_head_shape", "molds/").tip("Shape for making Hammer Heads");
+    public static ItemBasic<?> ShapeHeadFile = new ItemBasic<>(GTCore.ID, "file_head_shape", "molds/").tip("Shape for making File Heads");
+    public static ItemBasic<?> ShapeHeadSaw = new ItemBasic<>(GTCore.ID, "saw_head_shape", "molds/").tip("Shape for making Saw Heads");
+    public static ItemBasic<?> ShapeGear = new ItemBasic<>(GTCore.ID, "gear_shape", "molds/").tip("Shape for making Gears");
+    public static ItemBasic<?> ShapeGearSmall = new ItemBasic<>(GTCore.ID, "small_gear_shape", "molds/").tip("Shape for making Small Gears");
 
     public static void init(){
 
